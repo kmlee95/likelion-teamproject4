39fdd3d7da33fb5816505f6d8fa68a57abc393c2
wso0622 <yeon@yeonui-MacBook-Pro.local>
2019-07-19 14:32:27 +0900

    detail, write 추가

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Personal(models.Model):
+    title = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to='images/', null = True)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+
+    
