6b021ac7b15859ef552efce09bfda04dff995f6d
SoYeon <wso0622@gmail.com>
2019-07-25 12:08:29 +0900

    글쓰기버튼, 이미지 추가

--- likelionProject/settings.py
+++ likelionProject/settings.py
@@ -111,9 +111,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'personal', 'static'),
 ]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
